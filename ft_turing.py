e9de5b5c95e8a1c81738dba21ccd19f2b5ae5521
Axel Brisse <axel.brisse@gmail.com>
2024-02-11 23:33:35 +0100

    palindrome machine good but output in middle

--- ft_turing.py
+++ ft_turing.py
@@ -1,39 +1,45 @@
 import json
 import sys
 
+DEBUG = False
 MAX_STEPS = 10**3
+
+
+def dprint(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
 
 
 def stringify_tape(tape):
     return f"[{''.join(f'<{v}>' if k == pos else v for k,v in sorted(tape.items()))}]"
 
 
-print("*" * 80)
-print("*" + " " * 78 + "*")
-print("*" + sys.argv[1][:-5].center(78) + "*")
-print("*" + " " * 78 + "*")
-print("*" * 80)
+dprint("*" * 80)
+dprint("*" + " " * 78 + "*")
+dprint("*" + sys.argv[1][:-5].center(78) + "*")
+dprint("*" + " " * 78 + "*")
+dprint("*" * 80)
 machine = json.load(open(sys.argv[1]))
 transitions = {
     state: {t.pop("read"): t for t in ts}
     for state, ts in machine["transitions"].items()
 }
-print(f"Alphabet: [ {', '.join(machine['alphabet'])} ]")
-print(f"States: [ {', '.join(machine['states'])} ]")
-print(f"Initial: {machine['initial']}")
-print(f"Finals: [ {', '.join(machine['finals'])} ]")
+dprint(f"Alphabet: [ {', '.join(machine['alphabet'])} ]")
+dprint(f"States: [ {', '.join(machine['states'])} ]")
+dprint(f"Initial: {machine['initial']}")
+dprint(f"Finals: [ {', '.join(machine['finals'])} ]")
 for from_state, ts in transitions.items():
     for char, t in ts.items():
-        print(
+        dprint(
             f"({from_state}, {char}) -> ({t['to_state']}, {t['write']}, {t['action']})"
         )
 pos = 0
 state = machine["initial"]
 tape = dict(enumerate(sys.argv[2]))
-print("*" * 80)
+dprint("*" * 80)
 for _ in range(MAX_STEPS):
     if state in machine["finals"]:
-        print(stringify_tape(tape), "Finale state:", state)
+        print(stringify_tape(tape), "Final state:", state)
         break
     if pos not in tape:
         tape[pos] = machine["blank"]
@@ -41,7 +47,7 @@ for _ in range(MAX_STEPS):
     if step is None:
         print(stringify_tape(tape), f"Unexpected transition: ({state}, {tape[pos]})")
         break
-    print(
+    dprint(
         stringify_tape(tape),
         f"({state}, {tape[pos]})",
         "->",
