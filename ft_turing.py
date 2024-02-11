9bf950722706a29e7f7c2999562aa1c1d456f4be
Axel Brisse <axel.brisse@gmail.com>
2024-02-12 00:25:10 +0100

    main

--- ft_turing.py
+++ ft_turing.py
@@ -1,3 +1,4 @@
+from collections import deque
 import json
 import sys
 
@@ -10,52 +11,63 @@ def dprint(*args, **kwargs):
         print(*args, **kwargs)
 
 
-def stringify_tape(tape):
-    cells = sorted(tape.items(), reverse=True)
-    while cells and cells[-1][1] == machine["blank"]:
+def stringify_tape(tape, pos, blank):
+    cells = deque(sorted(tape.items()))
+    while cells and cells[0][1] == blank:
+        cells.popleft()
+    while cells and cells[-1][1] == blank:
         cells.pop()
-    return f"[{''.join(f'<{v}>' if k == pos else v for k,v in reversed(cells))}]"
+    return f"[{''.join(f'<{v}>' if k == pos else v for k,v in cells)}]"
 
 
-dprint("*" * 80)
-dprint("*" + " " * 78 + "*")
-dprint("*" + sys.argv[1][:-5].center(78) + "*")
-dprint("*" + " " * 78 + "*")
-dprint("*" * 80)
-machine = json.load(open(sys.argv[1]))
-transitions = {
-    state: {t.pop("read"): t for t in ts}
-    for state, ts in machine["transitions"].items()
-}
-dprint(f"Alphabet: [ {', '.join(machine['alphabet'])} ]")
-dprint(f"States: [ {', '.join(machine['states'])} ]")
-dprint(f"Initial: {machine['initial']}")
-dprint(f"Finals: [ {', '.join(machine['finals'])} ]")
-for from_state, ts in transitions.items():
-    for char, t in ts.items():
+def main():
+    dprint("*" * 80)
+    dprint("*" + " " * 78 + "*")
+    dprint("*" + sys.argv[1][:-5].center(78) + "*")
+    dprint("*" + " " * 78 + "*")
+    dprint("*" * 80)
+    machine = json.load(open(sys.argv[1]))
+    blank = machine["blank"]
+    transitions = {
+        state: {t.pop("read"): t for t in ts}
+        for state, ts in machine["transitions"].items()
+    }
+    dprint(f"Alphabet: [ {', '.join(machine['alphabet'])} ]")
+    dprint(f"States: [ {', '.join(machine['states'])} ]")
+    dprint(f"Initial: {machine['initial']}")
+    dprint(f"Finals: [ {', '.join(machine['finals'])} ]")
+    for from_state, ts in transitions.items():
+        for char, t in ts.items():
+            dprint(
+                f"({from_state}, {char}) -> ({t['to_state']}, {t['write']}, {t['action']})"
+            )
+    pos = 0
+    state = machine["initial"]
+    tape = dict(enumerate(sys.argv[2]))
+    dprint("*" * 80)
+    for _ in range(MAX_STEPS):
+        if state in machine["finals"]:
+            print(stringify_tape(tape, pos, blank), "Final state:", state)
+            break
+        if pos not in tape:
+            tape[pos] = machine["blank"]
+        step = transitions[state].get(tape[pos])
+        if step is None:
+            print(
+                stringify_tape(tape, pos, blank),
+                f"Unexpected transition: ({state}, {tape[pos]})",
+            )
+            break
         dprint(
-            f"({from_state}, {char}) -> ({t['to_state']}, {t['write']}, {t['action']})"
+            stringify_tape(tape, pos, blank),
+            f"({state}, {tape[pos]})",
+            "->",
+            f"({step['to_state']}, {step['write']}, {step['action']})",
         )
-pos = 0
-state = machine["initial"]
-tape = dict(enumerate(sys.argv[2]))
-dprint("*" * 80)
-for _ in range(MAX_STEPS):
-    if state in machine["finals"]:
-        print(stringify_tape(tape), "Final state:", state)
-        break
-    if pos not in tape:
-        tape[pos] = machine["blank"]
-    step = transitions[state].get(tape[pos])
-    if step is None:
-        print(stringify_tape(tape), f"Unexpected transition: ({state}, {tape[pos]})")
-        break
-    dprint(
-        stringify_tape(tape),
-        f"({state}, {tape[pos]})",
-        "->",
-        f"({step['to_state']}, {step['write']}, {step['action']})",
-    )
-    tape[pos] = step["write"]
-    pos += -1 if step["action"] == "LEFT" else 1
-    state = step["to_state"]
+        tape[pos] = step["write"]
+        pos += -1 if step["action"] == "LEFT" else 1
+        state = step["to_state"]
+
+
+if __name__ == "__main__":
+    main()
